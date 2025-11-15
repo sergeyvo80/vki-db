06a14cd461be7f44f9b4602a14f346f119d90836
Sergey Ovchinnikov <s.ovchinnikov@challenge-insure.ru>
2025-11-15 13:56:46 +0700

    fix: :fire: labs

--- labs/l1-BruceWillis.sql
+++
@@ -1,4 +0,0 @@
-SELECT Company.name FROM Company
-JOIN Passenger ON Passenger.name='Bruce Willis'
-JOIN Pass_in_trip ON Pass_in_trip.passenger=Passenger.id
-JOIN Trip ON Trip.id=Pass_in_trip.Trip AND Trip.company=Company.id;
